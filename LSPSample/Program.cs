6aaec24d3f0d61d66854ac7774466ae9fa40972b
Mani <137669778+Maniconserve@users.noreply.github.com>
2024-09-14 22:53:52 +0530

    Add files via upload

---
+++ LSPSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SOLID_PRINCIPLES.LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LSP Example - Square
+            Rectangle rect = new Square();
+            Console.WriteLine($"Square Area: {rect.GetPerimeter(5, 6)}");  
+        }
+    }
+
+    public class Rectangle
+    {
+        // Calculate area by passing width and height as parameters
+        public virtual int GetPerimeter(int length, int breadth)
+        {
+            return (2 * (length + breadth));
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        // Override to handle the square's unique area calculation
+        public override int GetPerimeter(int side, int unused)
+        {
+            return 4 * side;  // Since all sides of a square are equal
+        }
+    }
+}
