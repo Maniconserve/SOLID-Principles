c1fd040ae5163f4c0445477adcfd10a8fd73689e
Mani <137669778+Maniconserve@users.noreply.github.com>
2024-09-14 22:55:02 +0530

    Add files via upload

---
+++ OpenClosedSample/InvoiceOC.cs
@@ -0,0 +1,32 @@
+﻿namespace SOLID_PRINCIPLES.OCP
+{
+    public class InvoiceOC
+    {
+        public virtual double GetInvoiceDiscount(double amount)
+        {
+            return amount - 10;
+        }
+    }
+
+    public class FinalInvoice : InvoiceOC
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return base.GetInvoiceDiscount(amount) - 50;
+        }
+    }
+    public class ProposedInvoice : InvoiceOC
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return base.GetInvoiceDiscount(amount) - 40;
+        }
+    }
+    public class RecurringInvoice : InvoiceOC
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return base.GetInvoiceDiscount(amount) - 30;
+        }
+    }
+}
---
+++ OpenClosedSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SOLID_PRINCIPLES.OCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Invoice Amount: 10000");
+
+            InvoiceOC FInvoice = new FinalInvoice();
+            double FInvoiceAmount = FInvoice.GetInvoiceDiscount(10000);
+            Console.WriteLine($"Final Invoive : {FInvoiceAmount}");
+
+            InvoiceOC PInvoice = new ProposedInvoice();
+            double PInvoiceAmount = PInvoice.GetInvoiceDiscount(10000);
+            Console.WriteLine($"Proposed Invoive : {PInvoiceAmount}");
+
+            InvoiceOC RInvoice = new RecurringInvoice();
+            double RInvoiceAmount = RInvoice.GetInvoiceDiscount(10000);
+            Console.WriteLine($"Recurring Invoive : {RInvoiceAmount}");
+            Console.ReadKey();
+        }
+    }
+}
